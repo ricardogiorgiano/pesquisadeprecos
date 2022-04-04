ca9c54460caae657b4ff4079022f725cf83ac4ef
ricardogiorgiano <97055957+ricardogiorgiano@users.noreply.github.com>
2022-04-04 17:42:06 -0300

    commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+#automatização web
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+
+
+
+
+
+
+
