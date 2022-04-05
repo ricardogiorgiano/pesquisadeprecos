547d171d1122477ae9d92bf7eb39a52d81446939
ricardogiorgiano <97055957+ricardogiorgiano@users.noreply.github.com>
2022-04-05 09:27:32 -0300

    commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from itertools import count
+from mysqlx import Column, Row
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -6,6 +8,41 @@ from selenium import webdriver
 #automatização web
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+baseDados = "basededados.xlsx"
+
+
+UrlLA = "https://www.americanas.com.br/busca/"
+UrlMA = "https://www.magazineluiza.com.br/busca/"
+UrlGO = "https://www.google.com.br/search?tbm=shop&hl=pt-BR&psb=1&ved=2ahUKEwiW_9WUsOv2AhWRUEgAHUAeAQ0Qu-kFegQIABAK&q="
+
+
+
+df = pd.read_excel(baseDados)
+df.count
+
+
+
+for lin in range(len(df)):
+    
+    linkAM = ("")
+    UrlAM = "https://www.amazon.com.br/s?k="
+    chrome = webdriver.Chrome() #Carrega o Chrome
+    chrome.get(UrlAM,{lin}) #Entra na URL    
+    
+
+
+
+
+#for index,row in df.iterrows():
+#    print("Index: "+ str(index) + " Na linha tem o produto: " + row["Descrição do Produto"])
+#    chrome = webdriver.Chrome() #Carrega o Chrome
+
+#    chrome.quit #Sai do Chrome
+
+
+
+
 
 
 
